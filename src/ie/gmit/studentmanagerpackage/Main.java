e49e2d30a01b6090c36f56a11d7c5645df81aef8
danielcregg <daniel.cregg@gmit.ie>
2021-10-10 15:29:43 +0100

    Added code to test StudentManager class

--- src/ie/gmit/studentmanagerpackage/Main.java
+++ src/ie/gmit/studentmanagerpackage/Main.java
@@ -8,10 +8,24 @@ public class Main {
 	// Main method containing print statement.
 	public static void main(String[] args) {
 		
-		// Create menu object
-		Menu menuObject = new Menu();
-		// Run menu start method
-		menuObject.start();
+//		// Create menu object
+//		Menu menuObject = new Menu();
+//		// Run menu start method
+//		menuObject.start();
+		
+		StudentManager stuManObj = new StudentManager();
+		
+		Student studentObject1 = new Student("G00123456");
+		
+		boolean addStatus = stuManObj.addStudent(studentObject1);
+		
+		System.out.println(addStatus);
+		
+		boolean removeStatus = stuManObj.removeStudent(studentObject1);
+		
+		System.out.println(removeStatus);
+		
+		System.out.println("The End");
 		
 	} // End main method
 	
